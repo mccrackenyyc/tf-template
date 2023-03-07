10546a8841df5ae7cc56e772852badf8cdbce757
mccrackenyyc <smccracken@live.ca>
2023-03-07 14:43:35 -0700

    Updated convention for faster find and replace

--- base/resources.tf
+++ base/resources.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "name_statefile" {
-  name     = "name-statefile"
+resource "azurerm_resource_group" "updatename_statefile" {
+  name     = "updatename-statefile"
   location = "Canada Central"
 }
 
-resource "azurerm_storage_account" "name_statefile" {
-  name                     = "mccrackenyycNAMEstatefile"
-  resource_group_name      = azurerm_resource_group.name_statefile.name
-  location                 = azurerm_resource_group.name_statefile.location
+resource "azurerm_storage_account" "updatename_statefile" {
+  name                     = "mccrackenyycupdatenamestatefile"
+  resource_group_name      = azurerm_resource_group.updatename_statefile.name
+  location                 = azurerm_resource_group.updatename_statefile.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
@@ -15,8 +15,8 @@ resource "azurerm_storage_account" "name_statefile" {
   }
 }
 
-resource "azurerm_storage_container" "name_terraform" {
+resource "azurerm_storage_container" "updatename_terraform" {
   name                  = "terraform"
-  storage_account_name  = azurerm_storage_account.name_statefile.name
+  storage_account_name  = azurerm_storage_account.updatename_statefile.name
   container_access_type = "private"
 }
--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
     subscription_id      = var.subscription_id
-    resource_group_name  = "name-statefile"
-    storage_account_name = "mccrackenyycNAMEstatefile"
+    resource_group_name  = "updatename-statefile"
+    storage_account_name = "mccrackenyycupdatenamestatefile"
     container_name       = "terraform"
-    key                  = "name.terraform.tfstate"
+    key                  = "updatename.terraform.tfstate"
   }
 
   required_providers {
