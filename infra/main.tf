db408e1cfe7f96a8d6669e15236e95227627ebb2
mccrackenyyc <smccracken@live.ca>
2023-03-07 18:08:27 -0700

    Minor fix

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    subscription_id      = var.subscription_id
+    subscription_id      = "00000000-0000-0000-0000-000000000000"
     resource_group_name  = "updatename-statefile"
     storage_account_name = "mccrackenyycupdatenamestatefile"
     container_name       = "terraform"
